d6bd317e537667f88784b033eee855d7d8eebfed
xD3CODER <nathan.kremer@krakentechnologies.ltd>
2025-03-10 14:24:33 +0100

    del

--- expo-mesh-scanner.bkp/src/index.ts
+++
@@ -1,6 +0,0 @@
-// index.ts
-import MeshScannerModule from './ExpoMeshScannerModule';
-export default MeshScannerModule;
-
-export { default as ExpoMeshScannerView } from './ExpoMeshScannerView';
-export * from './ExpoMeshScannerModule';
